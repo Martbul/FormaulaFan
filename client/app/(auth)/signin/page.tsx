d64eaa339b6d408a19f4ccc21eb41b0379ba5b71
martbul <martbul01@gmail.com>
2024-08-02 00:30:14 +0300

    refactored auth
    now using react query and react hook form

--- client/app/(auth)/signin/page.tsx
+++ client/app/(auth)/signin/page.tsx
@@ -5,177 +5,184 @@ import {
   CardTitle,
   CardDescription,
   CardContent,
-  CardFooter
+  CardFooter,
 } from "@/components/ui/card";
-
 
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
-import { ChangeEvent, useState, FormEvent } from "react";
 import { useAuthContext } from "@/contexts/AuthContext2";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 import Link from "next/link";
-import { signIn } from "@/services/auth/auth.service";
 import { AnimatedCircleIcon, ChromeIcon, GithubIcon } from "@/utils/svgIcons";
-
+import { useForm } from "react-hook-form";
+import { useMutation } from "@tanstack/react-query";
+import { signIn } from "@/services/auth/auth.service";
+type SignInFormData = {
+  email: string;
+  password: string;
+};
 const Login = () => {
-    const router = useRouter()
-   const { setUser } = useAuthContext();
-   const [error, setError] = useState<string | null>(null);
-  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const { setUser } = useAuthContext();
 
-  const [form, setForm] = useState({
-    email: "",
-    password: "",
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignInFormData>();
+
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: async (data: SignInFormData) => {
+      const { email, password } = data;
+      const response = await signIn(email, password, setUser);
+      return response;
+    },
+    onSuccess: () => {
+      router.replace("/posts");
+    },
   });
 
-const handleChange = (
-  e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-) => {
-  const { id, value } = e.target;
-  setForm((prevForm) => ({
-    ...prevForm,
-    [id]: value,
-  }));
+  const onSubmit = (data: SignInFormData) => {
+    mutate(data);
+  };
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 w-full h-screen">
+      <div className="bg-zinc-950 flex flex-col items-center justify-center p-8">
+        <div className="max-w-md space-y-4">
+          <h1 className="text-4xl font-bold text-zinc-200">Formula Fan</h1>
+          <p className="text-lg text-gray-300">
+            Enjoy the world of Formula 1 from anothr angle
+          </p>
+          <div className="flex gap-4">
+            <Button
+              onClick={() => router.replace("/posts")}
+              variant="outline"
+              className="text-primary-foreground hover:bg-neutral-400"
+            >
+              Continue as Guest
+            </Button>
+
+            <Link
+              className="flex p-2 rounded-md text-sm font-medium items-center rounded bg-zinc-800 text-gray-200 hover:bg-neutral-600"
+              href="/signup"
+            >
+              Sign Up Now
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center p-8 bg-white">
+        <Card className="w-full max-w-md shadow-lg">
+          <CardHeader className="space-y-1">
+            <CardTitle className="text-2xl">Sing In</CardTitle>
+            <CardDescription>
+              Continue exploring our Formula 1 world today!
+            </CardDescription>
+          </CardHeader>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <CardContent className="grid gap-4">
+              <div className="grid grid-cols-2 gap-6">
+                <Button variant="outline">
+                  <GithubIcon className="mr-2 h-4 w-4" />
+                  Github
+                </Button>
+                <Button variant="outline">
+                  <ChromeIcon className="mr-2 h-4 w-4" />
+                  Google
+                </Button>
+              </div>
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                  <span className="w-full border-t" />
+                </div>
+                <div className="relative flex justify-center text-xs uppercase">
+                  <span className="bg-background px-2 text-muted-foreground">
+                    Or continue with email
+                  </span>
+                </div>
+              </div>
+
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email Address</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="example@email.com"
+                  className="border border-gray-950 p-2"
+                  {...register("email", {
+                    required: "Email is required",
+                    pattern: {
+                      value: /^[^@\s]+@[^@\s]+\.[^@\s]+$/,
+                      message: "Invalid email address",
+                    },
+                  })}
+                />
+                 {errors.email && (
+                  <span className="text-red-600">{errors.email.message}</span>
+                )}
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="mysecretpass"
+                  className="border border-gray-950 p-2"
+                 {...register("password", {
+                  required: "Password is required",
+                  minLength:{
+                    value:6,
+                    message:"Password must be at least 6 characters long"
+                  }
+                 })}
+                />
+                 {errors.password && (
+                  <span className="text-red-600">
+                    {errors.password.message}
+                  </span>
+                )}
+              </div>
+            </CardContent>
+            {isError && (
+              <div className="flex justify-center text-red-600 font-bold mb-4 px-8">
+                <p>{error.message}</p>
+              </div>
+            )}
+            <CardFooter>
+              <Button
+              type="submit"
+                className="w-full bg-zinc-950 text-white hover:bg-zinc-700 border border-black"
+                disabled={isPending}
+              >
+                {isPending  ? (
+                   <AnimatedCircleIcon className="h-9 w-9" />
+                 
+                ) : (
+                  "Sing In"
+                )}
+              </Button>
+            </CardFooter>
+          </form>
+
+          <div className="mb-4 text-center">
+            <p className="text-sm text-muted-foreground">
+              Don't have an account?{" "}
+              <Link
+                href="/signup"
+                className="font-medium underline"
+                prefetch={false}
+              >
+                Sign up
+              </Link>
+            </p>
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
 };
-
-  const handleSubmit = async (e: FormEvent) => {
-     setLoading(true);
-     e.preventDefault();
-     setError(null);
-
-     const result = await signIn(
-       form.username,
-       form.email,
-       form.password,
-       setUser
-     );
-
-     if (result.success) {
-       router.replace("/posts");
-     } else {
-       setError(result.message || "An error occurred during signim.");
-     }
-
-     setLoading(false);
-   };
-
- return (
-   <div className="grid grid-cols-1 md:grid-cols-2 w-full h-screen">
-     <div className="bg-zinc-950 flex flex-col items-center justify-center p-8">
-       <div className="max-w-md space-y-4">
-         <h1 className="text-4xl font-bold text-zinc-200">Formula Fan</h1>
-         <p className="text-lg text-gray-300">
-           Enjoy the world of Formula 1 from anothr angle
-         </p>
-         <div className="flex gap-4">
-           <Button
-           onClick={() =>  router.replace("/posts")}
-             variant="outline"
-             className="text-primary-foreground hover:bg-neutral-400"
-           >
-             Continue as Guest
-           </Button>
-
-           <Link
-             className="flex p-2 rounded-md text-sm font-medium items-center rounded bg-zinc-800 text-gray-200 hover:bg-neutral-600"
-             href="/signup"
-           >
-             Sign Up Now
-           </Link>
-         </div>
-       </div>
-     </div>
-     <div className="flex flex-col items-center justify-center p-8 bg-white">
-       <Card className="w-full max-w-md shadow-lg">
-         <CardHeader className="space-y-1">
-           <CardTitle className="text-2xl">Sing In</CardTitle>
-           <CardDescription>
-             Continue exploring our Formula 1 world today!
-           </CardDescription>
-         </CardHeader>
-         <CardContent className="grid gap-4">
-           <div className="grid grid-cols-2 gap-6">
-             <Button variant="outline">
-               <GithubIcon className="mr-2 h-4 w-4" />
-               Github
-             </Button>
-             <Button variant="outline">
-               <ChromeIcon className="mr-2 h-4 w-4" />
-               Google
-             </Button>
-           </div>
-           <div className="relative">
-             <div className="absolute inset-0 flex items-center">
-               <span className="w-full border-t" />
-             </div>
-             <div className="relative flex justify-center text-xs uppercase">
-               <span className="bg-background px-2 text-muted-foreground">
-                 Or continue with email
-               </span>
-             </div>
-           </div>
-
-           <div className="grid gap-2">
-             <Label htmlFor="email">Email Address</Label>
-             <Input
-               id="email"
-               type="email"
-               placeholder="example@email.com"
-               className="border border-gray-950 p-2"
-               value={form.email}
-               onChange={handleChange}
-               required
-             />
-           </div>
-           <div className="grid gap-2">
-             <Label htmlFor="password">Password</Label>
-             <Input
-               id="password"
-               type="password"
-               placeholder="mysecretpass"
-               className="border border-gray-950 p-2"
-               value={form.password}
-               onChange={handleChange}
-               required
-             />
-           </div>
-         </CardContent>
-            {error && (  <div className="flex justify-center text-red-600 font-bold mb-4 px-8"> 
-            <p>{error}</p>
-          </div>)}
-         <CardFooter>
-           <Button
-             className="w-full bg-zinc-950 text-white hover:bg-zinc-700 border border-black"
-             onClick={handleSubmit}
-           >
-             {loading === false ? (
-               "Sing In"
-             ) : (
-               <AnimatedCircleIcon className="h-9 w-9" />
-             )}
-           </Button>
-         </CardFooter>
-
-         <div className="mb-4 text-center">
-           <p className="text-sm text-muted-foreground">
-             Don't have an account?{" "}
-             <Link
-               href="/signup"
-               className="font-medium underline"
-               prefetch={false}
-             >
-               Sign up
-             </Link>
-           </p>
-         </div>
-       </Card>
-     </div>
-   </div>
- );
-}
-
 
 export default Login;
--- client/app/(auth)/signup/page.tsx
+++ client/app/(auth)/signup/page.tsx
@@ -12,151 +12,150 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { useAuthContext } from "@/contexts/AuthContext2";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useMutation } from "@tanstack/react-query";
 import { signUp } from "@/services/auth/auth.service";
-
 import Link from "next/link";
 import { AnimatedCircleIcon, ChromeIcon, GithubIcon } from "@/utils/svgIcons";
-import { useMutation } from "@tanstack/react-query";
+import { useAuthContext } from "@/contexts/AuthContext2";
 
+
+type SignUpFormData = {
+  username: string;
+  email: string;
+  password: string;
+};
 const SignUp = () => {
   const router = useRouter();
   const { setUser } = useAuthContext();
-  const [error, setError] = useState<string | null>(null);
-  const [loading, setLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpFormData>();
 
-  const [form, setForm] = useState({
-    username: "",
-    email: "",
-    password: "",
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: async (data:SignUpFormData) => {
+      const { username, email, password } = data;
+      const response = await signUp(username, email, password,setUser);
+      return response;
+    },
+    onSuccess: () => {
+      router.replace("/posts");
+    },
   });
 
-  const handleChange = (
-    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { id, value } = e.target;
-    setForm((prevForm) => ({
-      ...prevForm,
-      [id]: value,
-    }));
+  const onSubmit = (data:SignUpFormData) => {
+    mutate(data);
   };
-
-   const handleSubmit = async (e: FormEvent) => {
-     setLoading(true);
-     e.preventDefault();
-     setError(null);
-
-     const result = await signUp(
-       form.username,
-       form.email,
-       form.password,
-       setUser
-     );
-
-     if (result.success) {
-       router.replace("/posts");
-     } else {
-       setError(result.message || "An error occurred during signup.");
-     }
-
-     setLoading(false);
-   };
-
-
-  // const {mutate:signUpMutation} = useMutation(({username, email,password}) => signUp(username, email, password),{
-  //   onSuccess:(data) =>{
-  //     router.replace("/posts");
-  //   },
-  // })
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 w-full h-screen">
-      <div className=" flex flex-col items-center justify-center p-8 bg-white">
+      <div className="flex flex-col items-center justify-center p-8 bg-white">
         <Card className="w-full max-w-md shadow-lg">
           <CardHeader className="space-y-1">
-            <CardTitle className="text-2xl">Sing Up</CardTitle>
+            <CardTitle className="text-2xl">Sign Up</CardTitle>
             <CardDescription>
               Start exploring our Formula 1 world today!
             </CardDescription>
           </CardHeader>
-          <CardContent className="grid gap-4">
-            <div className="grid grid-cols-2 gap-6">
-              <Button variant="outline">
-                <GithubIcon className="mr-2 h-4 w-4" />
-                Github
-              </Button>
-              <Button variant="outline">
-                <ChromeIcon className="mr-2 h-4 w-4" />
-                Google
-              </Button>
-            </div>
-            <div className="relative">
-              <div className="absolute inset-0 flex items-center">
-                <span className="w-full border-t" />
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <CardContent className="grid gap-4">
+              <div className="grid grid-cols-2 gap-6">
+                <Button variant="outline">
+                  <GithubIcon className="mr-2 h-4 w-4" />
+                  Github
+                </Button>
+                <Button variant="outline">
+                  <ChromeIcon className="mr-2 h-4 w-4" />
+                  Google
+                </Button>
               </div>
-              <div className="relative flex justify-center text-xs uppercase">
-                <span className="bg-background px-2 text-muted-foreground">
-                  Or continue with email
-                </span>
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                  <span className="w-full border-t" />
+                </div>
+                <div className="relative flex justify-center text-xs uppercase">
+                  <span className="bg-background px-2 text-muted-foreground">
+                    Or continue with email
+                  </span>
+                </div>
               </div>
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="username">Username</Label>
-              <Input
-                id="username"
-                type="username"
-                placeholder="John Doe"
-                className="border border-gray-950 p-2"
-                value={form.username}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email Address</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="example@email.com"
-                className="border border-gray-950 p-2"
-                value={form.email}
-                onChange={handleChange}
-                required
-              />
-            </div>
-
-            <div className="grid gap-2">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                id="password"
-                type="password"
-                placeholder="mysecretpass"
-                className="border border-gray-950 p-2"
-                value={form.password}
-                onChange={handleChange}
-                required
-              />
-            </div>
-          </CardContent>
-          {error && (
-            <div className="flex justify-center text-red-600 font-bold mb-4 px-8">
-              <p>{error}</p>
-            </div>
-          )}
-          <CardFooter>
-            <Button
-              className="w-full bg-zinc-950 text-white hover:bg-zinc-700 border border-black"
-              onClick={handleSubmit}
-            >
-              {loading === false ? (
-                "Sing Up"
-              ) : (
-                <AnimatedCircleIcon className="h-9 w-9" />
-              )}
-            </Button>
-          </CardFooter>
-
+              <div className="grid gap-2">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  id="username"
+                  type="text"
+                  placeholder="John Doe"
+                  className="border border-gray-950 p-2"
+                  {...register("username", { required: "Username is required" })}
+                />
+                {errors.username && (
+                  <span className="text-red-600">
+                    {errors.username.message}
+                  </span>
+                )}
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email Address</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="example@email.com"
+                  className="border border-gray-950 p-2"
+                  {...register("email", {
+                    required: "Email is required",
+                    pattern: {
+                      value: /^[^@\s]+@[^@\s]+\.[^@\s]+$/,
+                      message: "Invalid email address",
+                    },
+                  })}
+                />
+                {errors.email && (
+                  <span className="text-red-600">{errors.email.message}</span>
+                )}
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="mysecretpass"
+                  className="border border-gray-950 p-2"
+                  {...register("password", {
+                    required: "Password is required",
+                    minLength: {
+                      value: 6,
+                      message: "Password must be at least 6 characters long",
+                    },
+                  })}
+                />
+                {errors.password && (
+                  <span className="text-red-600">
+                    {errors.password.message}
+                  </span>
+                )}
+              </div>
+            </CardContent>
+            {isError && (
+              <div className="flex justify-center text-red-600 font-bold mb-4 px-8">
+                <p>{error.message}</p>
+              </div>
+            )}
+            <CardFooter>
+              <Button
+                type="submit"
+                className="w-full bg-zinc-950 text-white hover:bg-zinc-700 border border-black"
+                disabled={isPending}
+              >
+                {isPending ? (
+                  <AnimatedCircleIcon className="h-9 w-9" />
+                ) : (
+                  "Sign Up"
+                )}
+              </Button>
+            </CardFooter>
+          </form>
           <div className="mb-4 text-center">
             <p className="text-sm text-muted-foreground">
               Already have an account?{" "}
@@ -175,7 +174,7 @@ const SignUp = () => {
         <div className="max-w-md space-y-4">
           <h1 className="text-4xl font-bold text-zinc-200">Formula Fan</h1>
           <p className="text-lg text-gray-300">
-            Enjoy the world of Formula 1 from anothr angle
+            Enjoy the world of Formula 1 from another angle
           </p>
           <div className="flex gap-4">
             <Button
@@ -187,7 +186,7 @@ const SignUp = () => {
             </Button>
 
             <Link
-              className="flex p-2 rounded-md text-sm font-medium items-center rounded bg-zinc-800 text-gray-200 hover:bg-neutral-600"
+              className="flex p-2 rounded-md text-sm font-medium items-center bg-zinc-800 text-gray-200 hover:bg-neutral-600"
               href="/signin"
             >
               Sign In Now
@@ -198,7 +197,5 @@ const SignUp = () => {
     </div>
   );
 };
-
-
 
 export default SignUp;
